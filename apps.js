3c0ddbfbf516a42ed4d934000ba617282406aff9
Ebuka2508 <ebukaenebeli971@gmail.com>
2020-11-23 15:34:57 +0100

    Online-Shopping1

---
+++ apps.js
@@ -0,0 +1,22 @@
+var counter = 1;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 4){
+        counter = 1;
+    }
+}, 5000);
+  
+ function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+
+
+
+
+ 
